021b0720c2aaa94e637b4e7755a086c703846173
Dinar2202 <Dinar2202@gmail.com>
2026-03-02 18:18:58 +0300

    feat: creat file dto

---
+++ src/rooms/dto/create-room-public.dto.ts
@@ -0,0 +1,22 @@
+import { IsArray, IsEnum, IsObject, IsString } from 'class-validator';
+import { RoomType } from '../entities/enity.rooms';
+import { ApiProperty } from '@nestjs/swagger';
+import { FirstMessageDto } from 'src/chat/dto/first-message.dto';
+
+export class CreateRoomPublicDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @IsEnum(RoomType)
+  @ApiProperty({ enum: RoomType, description: 'Тип комнаты (чата)' })
+  type: RoomType;
+
+  @ApiProperty()
+  @IsArray()
+  memberId: string[];
+
+  @ApiProperty()
+  @IsObject()
+  message: FirstMessageDto;
+}
